fab88acf0e08b60563d6cc952aabf586f0b363ca
allan <allanyanx@gmail.com>
2026-06-01 16:14:39 -0500

    Fix: search-bar. Feat: add shop, empty-state, product-card, product-skeleton. Models: add articulo, categoria. Services: add articulo.service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { Shop } from './features/shop/shop';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'shop', component: Shop }];
---
+++ src/app/features/shop/components/product-card/product-card.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { DecimalPipe } from '@angular/common'; // Importación ultra específica
+import { Articulo } from '../../../../core/models/articulo';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [DecimalPipe],
+  templateUrl: './product-card.html',
+})
+export class ProductCard {
+  articulo = input.required<Articulo>();
+}
