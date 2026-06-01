cce5aadcbc6fe586f25386e1e9e6f225b71cc866
allan <eraxoallan048@gmail.com>
2026-06-01 12:46:09 -0500

    shared: add header, search-bar, cart-widget. These components doesn't have functionality yet

---
+++ src/app/shared/components/cart-widget/cart-widget.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-widget',
+  imports: [],
+  templateUrl: './cart-widget.html',
+  styleUrl: './cart-widget.css',
+})
+export class CartWidget {}
