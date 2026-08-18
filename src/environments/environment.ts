b332375aa6c6bb730ff7700fc56fe78318cbb81b
Josue <josueidrovo123@gmail.com>
2026-08-18 11:23:21 -0500

    Comentarios para el entorno de produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
+  // IMPORTANTE: Cuando tengas tu dominio, cambia esta URL.
+  // Ejemplo: apiUrl: 'https://api.midominio.com/api',
   apiUrl: 'http://localhost:5104/api',
 };
